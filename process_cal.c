696cbbb097f613e49e55482430fce64ad972008f
Oniichann53 <hoangdt.5.3.2002@gmail.com>
2022-02-20 17:05:40 -0800

    commit

--- process_cal.c
+++ process_cal.c
@@ -24,9 +24,15 @@ typedef struct{
 
 FILE* openfile(char *argv[]);
 void readfile(FILE* fl, event evt[], int* sizep);
-void printdate(event evt[], char *argv[], int size);
+void final_event(event evt[], char *argv[], int size, event final_evt[], int *p);
 int compare_date (date d1, date d2);
+int compare_time(char time1[], char time2[]);
 void sort(event evt[], int size);
+void convert_date(date dt);
+void output(event evt[],int size);
+void convert_event(event evt);
+
+
 
 int main(int argc, char *argv[]) {
     event evt[MAX_EVENTS];
@@ -34,17 +40,10 @@ int main(int argc, char *argv[]) {
     int size = 0;
     readfile(fl, evt, &size);
     sort(evt, size);
-    printf("\n\n\n\n\n");
-    for (int i = 0; i < size; i++) {
-        printf("%s\n", evt[i].des);
-        printf("%s\n", evt[i].zone);
-        printf("%s\n", evt[i].loca);
-        printf("%d/%d/%d\n", evt[i].dt.year,evt[i].dt.month, evt[i].dt.day);
-        printf("%s\n", evt[i].dweek);
-        printf("%s\n", evt[i].start);
-        printf("%s\n", evt[i].end);
-    }
-    printdate(evt, argv, size);
+    event final_evt[MAX_EVENTS];
+    int final_size = 0;
+    final_event(evt, argv, size, final_evt, &final_size);
+    output(final_evt, final_size);
     return 0;
 }
 
@@ -75,50 +74,41 @@ void readfile(FILE* fl, event evt[], int* sizep) {
             strcpy(str, tok);
             if (strcmp("<description>", details) == 0) {
                 strcat (evt[i].des, str);
-                printf("%s\n", evt[i].des);
                 strcpy(details, "<timezone>");
             }
             else if (strcmp("<timezone>", details) == 0){
                 strcat (evt[i].zone, str);
-                printf("%s\n", evt[i].zone);
                 strcpy(details, "<location>");
             }
             else if (strcmp("<location>", details) == 0) {
                 strcat (evt[i].loca, str);
-                printf("%s\n", evt[i].loca);
                 strcpy(details, "<day>");
             }
             else if (strcmp("<day>", details) == 0) {
                 int numb = atoi(str);
                 evt[i].dt.day = numb;
-                printf("%d\n", evt[i].dt.day);
                 strcpy(details, "<month>");
             }
             else if (strcmp("<month>", details) == 0) {
                 int numb = atoi(str);
                 evt[i].dt.month = numb;
-                printf("%02d\n", evt[i].dt.month);
                 strcpy(details, "<year>");
             }
             else if (strcmp("<year>", details) == 0) {
                 int numb = atoi(str);
                 evt[i].dt.year =  numb;
-                printf("%02d\n", evt[i].dt.year);
                 strcpy(details, "<dweek>");
             }
             else if (strcmp("<dweek>", details) == 0) {
                 strcat (evt[i].dweek, str);
-                printf("%s\n", evt[i].dweek);
                 strcpy(details, "<start>");
             }
             else if (strcmp("<start>", details) == 0) {
                 strcat (evt[i].start, str);
-                printf("%s\n", evt[i].start);
                 strcpy(details, "<end>");
             }
             else if (strcmp("<end>",details) == 0) {
                 strcat (evt[i].end, str);
-                printf("%s\n", evt[i].end);
                 strcpy(details, "<description>");
             }
         }
@@ -126,44 +116,50 @@ void readfile(FILE* fl, event evt[], int* sizep) {
     *sizep = i;
 }
 
-void printdate(event evt[], char *argv[], int size){
+void final_event(event evt[], char *argv[], int size, event final_evt[], int *p){
     char ch[] = "=";
     char *temp = strtok(argv[1],ch);
     temp= strtok(NULL, ch);
     date d_s;
     evt[0] = evt[0];
     sscanf(temp, "%d/%d/%d", &d_s.year, &d_s.month, &d_s.day);
-    printf("%d",d_s.day);
     temp = strtok(argv[2],ch);
     temp= strtok(NULL, ch);
     date d_e;
     sscanf(temp, "%d/%d/%d", &d_e.year, &d_e.month, &d_e.day);
-    printf("\n%d",size);
-    event temp_evt[MAX_EVENTS];
-    event *temp_evt_p = &temp_evt[0];
+    event *final_evt_p = &final_evt[0];
     for (int i = 0; i<size; i++) {
         if ((compare_date(evt[i].dt, d_s) == 1) || (compare_date(evt[i].dt, d_s) == 0)) {
             if ((compare_date(evt[i].dt, d_e) == -1) || (compare_date(evt[i].dt, d_e) == 0)) {
-                *temp_evt_p = evt[i];
-                printf(temp_evt[0].des);
+                *final_evt_p = evt[i];
+                (*p)++;
+                final_evt_p++;
             }
         }
     }
+    
 }
 
 void sort(event evt[], int size) {
     int numb = 0;
     while (numb < size){
         for (int i = size-1; i >0; i--) {
-            if (compare_date(evt[i-1].dt,evt[i].dt) == 1) {
+            if(compare_date(evt[i-1].dt,evt[i].dt) == 1) {
                 event temp = evt[i];
                 evt[i] = evt[i-1];
                 evt[i-1] = temp;
+            } else if (compare_date(evt[i-1].dt,evt[i].dt) == 0){
+            	if (compare_time(evt[i-1].start, evt[i].start) == 1) {
+            		event temp = evt[i];
+                	evt[i] = evt[i-1];
+                	evt[i-1] = temp;
+                }
             }
         }
         numb++;
     }
 }
+
 int compare_date (date d1, date d2) {
     if (d1.year > d2.year) {
         return 1;
@@ -187,3 +183,111 @@ int compare_date (date d1, date d2) {
     }
 }
 
+int compare_time(char time1[], char time2[]) {
+    int hr1;
+    int min1;
+    int hr2;
+    int min2;
+	sscanf(time1, "%d:%d", &hr1, &min1);
+	sscanf(time2, "%d:%d", &hr2, &min2);
+	if (hr1 > hr2) {
+		return 1;
+	} else if (hr1 < hr2) {
+		return -1;
+	} else {
+        if (min1 > min2) {
+            return 1;
+        } else if (min1 < min2) {
+            return -1;
+        } else {
+            return 0;
+        }
+	}
+}
+
+void convert_date(date dt) {
+    if (dt.month == 1) {
+        printf("%d January, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 2) {
+        printf("%d Febuary, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 3) {
+        printf("%d March, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 4) {
+        printf("%d April, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 5) {
+        printf("%d May, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 6) {
+        printf("%d June, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 7) {
+        printf("%d July, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 8) {
+        printf("%d August, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 9) {
+        printf("%d September, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 10) {
+        printf("%d October, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 11) {
+        printf("%d November, %d\n", dt.day, dt.year);
+    }
+    if (dt.month == 12) {
+        printf("%d December, %d\n", dt.day, dt.year);
+    }
+}
+
+void convert_event(event evt) {
+    int hr_s;
+    int min_s;
+    int hr_e;
+    int min_e;
+    char ch_s[50];
+    char ch_e[50];
+    char am[] = "AM";
+    char pm[] = "PM";
+	sscanf(evt.start, "%d:%d", &hr_s, &min_s);
+	sscanf(evt.end, "%d:%d", &hr_e, &min_e);
+    if (hr_s < 12) {
+        strcpy(ch_s, am);
+    } else {
+        strcpy(ch_s, pm);
+        if (hr_s != 12) {
+            hr_s -= 12;
+        }
+    }
+    if (hr_e < 12) {
+        strcpy(ch_e, am);
+    } else {
+        strcpy(ch_e, pm);
+        if (hr_e != 12) {
+            hr_e -= 12;
+
+        }
+    }
+    //printf("\n%s, %s\n",ch_s, ch_e);
+    printf("%02d:%02d %s to %02d:%02d %s: %s {{%s}} | %s\n", hr_s, min_s, ch_s, hr_e, min_e, ch_e, evt.des, evt.loca, evt.zone);
+}
+
+void output(event evt[], int size) {
+    convert_date(evt[0].dt);
+    printf("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐\n");
+    convert_event(evt[0]);
+    for (int i = 1; i < size; i++) {
+        if (compare_date(evt[i].dt,evt[i-1].dt) == 0) {
+            convert_event(evt[i]);
+        } else {
+            printf("\n");
+            convert_date(evt[i].dt);
+            printf("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐\n");
+            convert_event(evt[i]);
+        }
+    }
+}
---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    char ch[] = "blablabla";
+    printf("%s", ch);
+    return 0;
+}
